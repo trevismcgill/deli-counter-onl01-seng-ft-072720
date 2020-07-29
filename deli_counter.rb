5815a2c9b63e8fb02dcf67dd94862270ad6aa05a
Trevis McGill <trevismcgill25@gmail.com>
2020-07-29 18:05:31 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+require "pry"
+katz_deli = []
+
+def take_a_number(katz_deli, name)
+        katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+def line(katz_deli)
+  line_preface = "The line is currently: "
+  line = []
+    if katz_deli.length == 0
+      puts "The line is currently empty."
+
+    else katz_deli.each_with_index do |name, index|
+      line.push("#{index + 1}. #{name}")
+    end
+    puts line_preface + line.join(" ")
+  end
+end
+
+def now_serving(array)
+  if array.length > 0
+      puts "Currently serving #{array.first}."
+      array.shift
+
+  else
+    puts "There is nobody waiting to be served!"
+  end
+end
