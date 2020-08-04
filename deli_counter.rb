ce40b66a27427baa7249146a2b4048a1663cc017
Trevis McGill <trevismcgill25@gmail.com>
2020-08-04 02:42:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,16 +20,16 @@ def line(katz_deli)
 end
 
 def now_serving(array)
-  # if array.length > 0
-  #     puts "Currently serving #{array.first}."
-  #     array.shift
-
-if array.length > 0
-    array.each do |name|
-      puts "Currently serving #{name}."
-      binding.pry
+  if array.length > 0
+      puts "Currently serving #{array.first}."
       array.shift
-  end
+
+# if array.length > 0
+#     array.each do |name|
+#       puts "Currently serving #{name}."
+#       binding.pry
+#       array.shift
+#   end
 else
   puts "There is nobody waiting to be served!"
 end
